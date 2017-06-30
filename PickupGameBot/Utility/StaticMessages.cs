313994984b5c2e006742468c986fdb3b601eb00d
Elliot DeNolf <denolfe@gmail.com>
2017-06-30 01:32:56 -0600

    Add help message

---
+++ PickupGameBot/Utility/StaticMessages.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+
+namespace PickupGameBot.Utility
+{
+    public static class StaticMessages
+    {
+        public static EmbedBuilder Welcome() 
+            => new EmbedBuilder()
+                .WithColor(new Color(0, 255, 0))
+    
+                .AddField(new EmbedFieldBuilder()
+                    .WithName("Admin Setup Commands")
+                    .WithValue("!teamsize, !pickmode, !captainmode"))
+                
+                .AddField(new EmbedFieldBuilder()
+                    .WithName("Player Commands")
+                    .WithValue("!join, !leave, !status"));
+    }
+}
